7359c0e6cf974efdeac95eaf9b977bcc96fad091
Lucky <turboluckyc@gmail.com>
2022-04-04 21:36:20 -0400

    Starting the admin-panel, almost done with home.

--- spell/urls.py
+++ spell/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("confirm-admin", views.confirm, name="confirm"),
+    path("admin_panel", views.admin_panel, name="admin_panel"),
 ]
--- spell/views.py
+++ spell/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
 def index(request):
-    return render(request, "spell/index.html")
+    if "pin" not in request.session or request.session["pin"] == "" or request.session["pin"] == "CONFIRMED":
+        return render(request, "spell/index.html", {
+            "message": ""
+        })
+    else:
+        request.session["pin"] = ""
+        return render(request, "spell/index.html", {
+            "message": "Invalid PIN!"
+        })
+
+def confirm(request):
+    pin = request.POST["pin"]
+    if pin != "6911":
+        request.session["pin"] = "UNCONFIRMED"
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        request.session["pin"] = "CONFIRMED"
+        return HttpResponseRedirect(reverse("admin_panel"))
+
+def admin_panel(request):
+    if "pin" not in request.session or request.session["pin"] == "" or request.session["pin"] == "CONFIRMED":
+        return render(request, "spell/words.html", {
+            "message": ""
+        })
+    else:
+        request.session["pin"] = ""
+        return render(request, "spell/words.html", {
+            "message": "Invalid PIN!"
+        })
